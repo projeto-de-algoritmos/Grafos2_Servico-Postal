92105827ec105a944e4d14bb92c05abe5cc59a10
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-02-18 16:25:54 -0300

    Create basic body of the project with Dijkstra Algorithm
    Co-authored-by: Juliana Valle <juju.pvg@gmail.com>

---
+++ PostalServices/Models/Cities.cs
@@ -0,0 +1,9 @@
+﻿namespace PostalServices.Models
+{
+    public class Cities
+    {
+        public int codePostal { get; set; }
+        public int hours { get; set; }
+        public int acord { get; set; }
+    }
+}
---
+++ PostalServices/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PostalServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+// como assim?
