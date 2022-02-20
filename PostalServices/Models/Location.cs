ec12a80ca35dd6927182cff0f7b980670070627e
Juliana Valle <juju.pvg@gmail.com>
2022-02-20 15:32:48 -0300

    Add Location Model
    Co-authored-by: Matheus Monteiro <matheusyanmonteiro@gmail.com>

---
+++ PostalServices/Models/Location.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace PostalServices.Models
+{
+    public class Location
+    {
+        [Key]
+        public int LocId { get; set; }
+
+        [Display(Name = "Description"), MaxLength(8000, ErrorMessage = "Title cannot be more than 8000 characters long."), Required(ErrorMessage = "You must enter a description in {0} field")]
+        [AllowHtml]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [Display(Name = "Park/Location Name"), MaxLength(120, ErrorMessage = "Park or Location name is too long"), Required]
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N10}", ApplyFormatInEditMode = true), Required]
+        public decimal Longitude { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N10}", ApplyFormatInEditMode = true), Required]
+        public decimal Latitude { get; set; }
+    }
+}
